11eb8c5b6dee8876471e8be513809baeff363ebf
Edgxxar <edgxxar@gmail.com>
2018-11-23 15:45:13 +0100

    added config.properties file for NS instance configuration properly use query parameters in rest access

--- code/OpenDiabetesVault-NSApi/src/de/opendiabetes/vault/nsapi/Main.java
+++ code/OpenDiabetesVault-NSApi/src/de/opendiabetes/vault/nsapi/Main.java
@@ -4,13 +4,32 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParser;
 
+import java.io.*;
+import java.util.Properties;
+
 public class Main {
     private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private static JsonParser parser = new JsonParser();
 
     public static void main(String[] args) {
-        NSApi api = new NSApi("https://edgxxar.ns.10be.de:15109");
-        printJson(api.getEntries().spec("sgv").find("dateString").gte("2018-11-08T15:05:00").get());
+        String host, port, token;
+        
+        try (InputStream input = new FileInputStream("resources/config.properties")) {
+            Properties properties = new Properties();
+            properties.load(input);
+            
+            host = properties.getProperty("host");
+            port = properties.getProperty("port");
+            token = properties.getProperty("token");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+            return;
+        }
+
+        NSApi api = new NSApi(host, port, token);
+        printJson(api.getStatus());
+        printJson(api.getEntries().get());
     }
 
     private static void printJson(String jsonString) {
--- code/OpenDiabetesVault-NSApi/src/de/opendiabetes/vault/nsapi/NSApi.java
+++ code/OpenDiabetesVault-NSApi/src/de/opendiabetes/vault/nsapi/NSApi.java
@@ -2,42 +2,23 @@ package de.opendiabetes.vault.nsapi;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
 
 public class NSApi {
-    WebTarget target;
+    private Client client;
+    private String target;
+    private String token;
 
-    public NSApi(String host) {
-        Client client = ClientBuilder.newClient();
-
-        target = client.target(host + "/api/v1");
-    }
-
-    private Invocation.Builder buildPath(String path) {
-        Invocation.Builder b = target.path(path).request(MediaType.APPLICATION_JSON);
-        return b;
+    public NSApi(String host, String port, String token) {
+        this.client = ClientBuilder.newClient();
+        this.target = "https://" + host + ":" + port + "/api/v1";
+        this.token = token;
     }
 
     public String getStatus() {
-        return buildPath("status").get(String.class);
+        return new GetBuilder(client, target, "status").token(token).get();
     }
 
-    public GetEntriesBuilder getEntries() {
-        return new GetEntriesBuilder(this);
-    }
-    
-    public String get(String path) {
-        System.out.println(path);
-        return buildPath(path).get(String.class);
-    }
-    
-    public String getProfile() {
-        return buildPath("profile").get(String.class);
-    }
-    
-    public String getTreatments() {
-        return buildPath("treatments").get(String.class);
+    public GetBuilder getEntries() {
+        return new GetBuilder(client, target, "entries").token(token);
     }
 }
