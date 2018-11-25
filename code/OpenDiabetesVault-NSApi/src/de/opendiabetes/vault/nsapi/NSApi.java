cad481dfa06e4b3b81608cbffee0e8a2ee0d9d15
Edgxxar <edgxxar@gmail.com>
2018-11-25 18:17:08 +0100

    added POST entries method

--- code/OpenDiabetesVault-NSApi/src/de/opendiabetes/vault/nsapi/NSApi.java
+++ code/OpenDiabetesVault-NSApi/src/de/opendiabetes/vault/nsapi/NSApi.java
@@ -2,6 +2,8 @@ package de.opendiabetes.vault.nsapi;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 
 public class NSApi {
     private Client client;
@@ -21,4 +23,13 @@ public class NSApi {
     public GetBuilder getEntries() {
         return new GetBuilder(client, target, "entries").token(token);
     }
+
+    public GetBuilder getSlice(String storage, String field, String type, String prefix, String regex) {
+        return new GetBuilder(client, target, "slice/" + storage + "/" + field + "/" + type + "/" + prefix + "/" + regex).token(token);
+    }
+
+    public String postEntries(String entries) {
+        return client.target(target).path("entries").queryParam("token", token).request(MediaType.APPLICATION_JSON).post(Entity.json(entries), String.class);
+    }
+
 }
