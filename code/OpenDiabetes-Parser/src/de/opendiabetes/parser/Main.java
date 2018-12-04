c920e50ded3bde6d0e50c9f3b78625856b3c461d
Paul Wolfart <paul.wolfart@gmail.com>
2018-12-04 21:00:47 +0100

    Main

--- code/OpenDiabetes-Parser/src/de/opendiabetes/parser/Main.java
+++ code/OpenDiabetes-Parser/src/de/opendiabetes/parser/Main.java
@@ -1,17 +1,32 @@
 package de.opendiabetes.parser;
 
-import java.io.FileNotFoundException;
+import de.opendiabetes.vault.engine.container.VaultEntry;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args){
-        JSONParser parser = new JSONParser();
-        
+        VaultEntryParser parser = new VaultEntryParser();
+        StringBuilder jsonFile = new StringBuilder();
+        List<VaultEntry> entries;
+        String path = "";
 
+        try (Stream<String> stream = Files.lines( Paths.get(path), StandardCharsets.UTF_8)) {
+            stream.forEach(line -> jsonFile.append(line));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-
-
-
+        entries = parser.parse(jsonFile.toString());
+        System.out.println("Entries:");
+        for (VaultEntry entry : entries){
+            System.out.println(entry.toString());
+        }
 
     }
 
