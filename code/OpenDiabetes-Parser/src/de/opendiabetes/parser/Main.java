aad062ed4d688ba372537f52c225f61778e0805c
Paul Wolfart <paul.wolfart@gmail.com>
2018-12-10 16:32:22 +0100

    cleanup

--- code/OpenDiabetes-Parser/src/de/opendiabetes/parser/Main.java
+++ code/OpenDiabetes-Parser/src/de/opendiabetes/parser/Main.java
@@ -6,25 +6,23 @@ import java.util.List;
 
 public class Main {
 
-    private static final long ONE_HOUR = 3600000;
-
     public static void main(String[] args) {
         VaultEntryParser parser = new VaultEntryParser();
 
-        String path = "/home/anna/Daten/Uni/14. Semester/BP/Dataset_Small/00390014/direct-sharing-31/entries_2017-07-10_to_2017-11-08.json";
+
+        String path = "";
         List<VaultEntry> entries = parser.parseFile(path);
         System.out.println("Entries:");
         for (VaultEntry entry : entries) {
             System.out.println(entry.toString());
         }
 
-        String path2 = "/home/anna/Daten/Uni/14. Semester/BP/Dataset_Small/00390014/direct-sharing-31/treatments_2017-07-10_to_2017-11-08.json";
+        String path2 = "";
         List<VaultEntry> treatments = parser.parseFile(path2);
         System.out.println("Treatments:");
         for (VaultEntry entry : treatments) {
             System.out.println(entry.toString());
         }
 
-        System.out.println("Timezone: GMT " + parser.getTimezone());
     }
 }
