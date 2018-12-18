7dfdf1ce4e0a90bc9326e39858c9fbbaaa65fda7
Jan Petto <petto@ref010.sm.asta.tu-darmstadt.de>
2018-12-18 13:50:26 +0100

    WIP: implemented synchronizer / untested

--- code/OpenDiabetes-Synchronizer/src/de/opendiabetes/synchronizer/Main.java
+++ code/OpenDiabetes-Synchronizer/src/de/opendiabetes/synchronizer/Main.java
@@ -1,5 +1,7 @@
 package de.opendiabetes.synchronizer;
 
+import com.google.gson.*;
+import de.opendiabetes.vault.nsapi.GetBuilder;
 import de.opendiabetes.vault.nsapi.NSApi;
 
 import java.io.FileInputStream;
@@ -14,6 +16,8 @@ public class Main {
         String configpath = "config.properties";
         String start = null;
         String end = null;
+        String batchsize = null;
+        int count = 100;
         try {
             for (int i = 0; i < args.length; i++) {
                 String arg = args[i];
@@ -28,6 +32,10 @@ public class Main {
                             break;
                         case "end":
                             end = getValue(arg, args, i);
+                            break;
+                        case "batch":
+                        case "batchsize":
+                            batchsize = getValue(arg, args, i);
                             break;
                         default:
                             throw new IllegalArgumentException("Unknown argument " + arg);
@@ -44,6 +52,13 @@ public class Main {
                 throw new IllegalArgumentException("start date missing or invalid");
             if (end != null && !timepattern.matcher(end).find())
                 throw new IllegalArgumentException("end date invalid");
+            if (batchsize != null) {
+                try {
+                    count = Integer.parseInt(batchsize);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("Invalid batchsize " + batchsize);
+                }
+            }
         } catch (IllegalArgumentException e) {
             System.out.println("Error while parsing arguments: " + e.getMessage());
             return;
@@ -73,10 +88,46 @@ public class Main {
         NSApi read = new NSApi(readHost, readPort, readToken);
         NSApi write = new NSApi(writeHost, writePort, writeToken);
 
-        //TODO: do the work
+        GetBuilder getBuilder;
+        JsonParser parser = new JsonParser();
+        JsonArray found = new JsonArray();
+        JsonArray missing = new JsonArray();
+        int fc = 0;
+        do {
+            getBuilder = read.getEntries().count(count);
+
+            if (found.size() == 0) {  // first search
+                getBuilder.find("dateString").gte(start);
+            } else {    // next search, start at last found entry
+                String last = found.get(found.size() - 1).getAsJsonObject().get("dateString").getAsString();
+                getBuilder.find("dateString").gt(last);
+            }
+
+            if (end != null)
+                getBuilder.find("dateString").lt(end);
+
+            found = parser.parse(getBuilder.get()).getAsJsonArray();
+            fc += found.size();
+
+            for (JsonElement e : found) {
+                JsonObject entry = e.getAsJsonObject();
+                String date = entry.get("dateString").getAsString();
+                String target = write.getEntries().find("dateString").eq(date).get();
+                if (target.equals("[]"))    //no result found
+                    missing.add(entry);
+            }
+        } while (found.size() != 0);
+
+        System.out.println("Found " + fc + " entries of which " + missing.size() + " are missing in the target instance.");
+        if (missing.size() > 0) {
+            System.out.println("Uploading missing entries...");
+            Gson gson = new Gson();
+            write.postEntries(gson.toJson(missing));
+        }
 
         read.close();
         write.close();
+        System.out.println("Done!");
     }
 
     private static String getValue(String arg, String[] args, int i) {
