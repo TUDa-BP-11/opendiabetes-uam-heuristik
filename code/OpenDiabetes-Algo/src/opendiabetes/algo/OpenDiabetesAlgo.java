585e0b66dbae81d6929a605a67a6b715ecd57817
Paul Wolfart <paul.wolfart@gmail.com>
2018-12-23 16:07:10 +0100

    predict

--- code/OpenDiabetes-Algo/src/opendiabetes/algo/OpenDiabetesAlgo.java
+++ code/OpenDiabetes-Algo/src/opendiabetes/algo/OpenDiabetesAlgo.java
@@ -10,23 +10,19 @@ package opendiabetes.algo;
 import de.opendiabetes.vault.engine.container.VaultEntry;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-/**
- * @author anna
- */
+
 public class OpenDiabetesAlgo {
 
 
-    private double absorptionTime;
-    private int insDuration;
+    private final double absorptionTime = 120;
+    private final int insDuration = 3;
     private double carbRatio;
     private double insSensivityFactor;
     private List<VaultEntry> glucose;
     private List<VaultEntry> bolusTreatments;
     private List<VaultEntry> mealTreatments;
-    private List<VaultEntry> output;
     private VaultEntry current;
     double startValue;
 
@@ -37,14 +33,11 @@ public class OpenDiabetesAlgo {
      */
 
     OpenDiabetesAlgo() {
-        absorptionTime = 180;
-        insDuration = 6 ;
         carbRatio = 5;
         insSensivityFactor = 10;
         glucose = new ArrayList<VaultEntry>();
         bolusTreatments = new ArrayList<VaultEntry>();
         mealTreatments = new ArrayList<VaultEntry>();
-        output = new ArrayList<VaultEntry>();
     }
 
     public void setGlucose(List<VaultEntry> glucose) {
@@ -52,18 +45,35 @@ public class OpenDiabetesAlgo {
         startValue = current.getValue();
         bolusTreatments = new ArrayList<VaultEntry>();
         mealTreatments  = new ArrayList<VaultEntry>();
-        output  = new ArrayList<VaultEntry>();
         this.glucose = glucose;
     }
 
-    public void addGlucose(List<VaultEntry> glucose) {
-        this.glucose.addAll(glucose);
+    public List<VaultEntry> calc() {
+
+
+        return mealTreatments;
+
     }
 
-    public List<VaultEntry> getOutput() {
-        List<VaultEntry> tmp = output;
-        output = new ArrayList<VaultEntry>();
-        return tmp;
+    private double predict(long time){
+        double result = startValue;
+        for (VaultEntry meal: mealTreatments){
+            long deltaTime = Math.round((time - meal.getTimestamp().getTime())/60000.0);//Time in minutes
+            if(deltaTime<=0){
+                break;
+            }
+            result += deltaBGC(deltaTime,insSensivityFactor,carbRatio,meal.getValue(),absorptionTime);
+        }
+        for (VaultEntry bolus: bolusTreatments){
+            long deltaTime = Math.round((time - bolus.getTimestamp().getTime())/60000.0);//Time in minutes
+            if(deltaTime<=0){
+                break;
+            }
+            result += deltaBGI(deltaTime,bolus.getValue(),insSensivityFactor,insDuration);
+        }
+
+
+        return result;
     }
 
     /**
@@ -74,32 +84,16 @@ public class OpenDiabetesAlgo {
      * @return
      */
     //function iob(g,idur)
-    public double getIOBWeight(double timeFromEvent, int insDuration) {
-        double IOBWeight = 0;
+    public int getIOBWeight(double timeFromEvent, int insDuration) {
+        int IOBWeight = 0;
         if (timeFromEvent <= 0) {
             IOBWeight = 100;
         } else if (timeFromEvent >= insDuration * 60) {
             IOBWeight = 0;
         } else {
-            switch (insDuration) {
-                case 3:
-                    IOBWeight = -3.203e-7 * Math.pow(timeFromEvent, 4) + 1.354e-4 * Math.pow(timeFromEvent, 3) - 1.759e-2 * Math.pow(timeFromEvent, 2) + 9.255e-2 * timeFromEvent + 99.951;
-                    break;
-                case 4:
-                    IOBWeight = -3.31e-8 * Math.pow(timeFromEvent, 4) + 2.53e-5 * Math.pow(timeFromEvent, 3) - 5.51e-3 * Math.pow(timeFromEvent, 2) - 9.086e-2 * timeFromEvent + 99.95;
-                    break;
-                case 5:
-                    IOBWeight = -2.95e-8 * Math.pow(timeFromEvent, 4) + 2.32e-5 * Math.pow(timeFromEvent, 3) - 5.55e-3 * Math.pow(timeFromEvent, 2) + 4.49e-2 * timeFromEvent + 99.3;
-                    break;
-                case 6:
-                    IOBWeight = -1.493e-8 * Math.pow(timeFromEvent, 4) + 1.413e-5 * Math.pow(timeFromEvent, 3) - 4.095e-3 * Math.pow(timeFromEvent, 2) + 6.365e-2 * timeFromEvent + 99.7;
-                    break;
-                case 8: // TODO Jens fragen
-                default:
-                    break;
-            }
+            IOBWeight =  (int) (-3.203e-7 * Math.pow(timeFromEvent, 4) + 1.354e-4 * Math.pow(timeFromEvent, 3) - 1.759e-2 * Math.pow(timeFromEvent, 2) + 9.255e-2 * timeFromEvent + 99.951);
         }
-        return (IOBWeight);
+        return IOBWeight;
     }
 
 
@@ -125,10 +119,7 @@ public class OpenDiabetesAlgo {
         return integral;
 
     }
-
-
-    //scheiner gi curves fig 7-8 from Think Like a Pancreas, fit with a triangle shaped absorbtion rate curve
-    //see basic math pdf on repo for details
+    
     //g is time in minutes,gt is carb type
     //function cob(g,ct)
     public double cob(double timeFromEvent, double absorptionTime) {
@@ -165,67 +156,5 @@ public class OpenDiabetesAlgo {
         return deltaBGI(timeFromEvent, insBolus, insSensivityFactor, insDuration) +
                 deltaBGC(timeFromEvent, insSensivityFactor, carbRatio, carbsAmount, absorptionTime);
     }
-
-
-
-
-
-
-    /*
-
-    /**
-     * @param args the command line arguments
-     * @param IOB Insulin on board
-     * @param I_G Insulin need to correct for posoitve deviation
-     * @param I_CHO amount  of  insulin  needed to  compensate  for  a  given  meal
-     */
-/*    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-
-    public double getUmax(double I_CHO) {
-        double IOB = this.getIOB();
-        double I_G = this.getIG();
-        
-        if(I_CHO + I_G> IOB)
-            return I_CHO + I_G - IOB;
-        else
-            return I_CHO;
-    }
-
-    public double getIOB(Date now, long time) {
-        double IOB = 0;
-        double dt = 0;
-
-        recent = this.vault.get(insulin + time).(now, time);
-        for (int i = 0; i < length(recent); i++) {
-            dt = now - recent(i).time;
-            IOB += recent(i).insulin * getIOBWeight(dt, time);
-        }
-    }
-        return IOB;
-    }
-
-    //sum IOB 
-    //Idea without integration
-    public double sumIOB(double x1, double x2, int iDuration, double timeFromEvent) {
-        double integral;
-        double dx;
-        int nn = 50;
-
-        //initialize with first and last terms of simpson series
-        dx = (x2 - x1) / nn;
-        integral = getIOBWeight((timeFromEvent - x1), iDuration) + getIOBWeight(timeFromEvent - (x1 + nn * dx), iDuration);
-
-        for (int ii = 0; ii < nn - 2; ii++) {
-            integral = integral + 4 * getIOBWeight(timeFromEvent - (x1 + ii * dx), iDuration) + 2 * getIOBWeight(timeFromEvent - (x1 + (ii + 1) * dx), iDuration);
-            ii = ii + 2;
-        }
-
-        integral = integral * dx / 3.0;
-        return integral;
-
-    }*/
-
 
 }
