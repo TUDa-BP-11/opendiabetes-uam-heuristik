b781c6c0e8a0d70a303a6c0ce38e39fae26bdc59
Edgxxar <edgxxar@gmail.com>
2019-03-13 17:12:31 +0100

    moved package to correct path, added formatter tests

---
+++ code/OpenDiabetes-NSApi/src/de/opendiabetes/vault/parser/Profile.java
@@ -0,0 +1,53 @@
+package de.opendiabetes.vault.parser;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.List;
+
+public class Profile {
+    private ZoneId timezone;
+    private double sensitivity;
+    private double carbratio;
+    private List<BasalProfile> basalProfiles;
+
+    public Profile(ZoneId timezone, double sensitivity, double carbratio, List<BasalProfile> basalProfiles) {
+        this.timezone = timezone;
+        this.sensitivity = sensitivity;
+        this.carbratio = carbratio;
+        this.basalProfiles = basalProfiles;
+    }
+
+    public ZoneId getTimezone() {
+        return timezone;
+    }
+
+    public double getSensitivity() {
+        return sensitivity;
+    }
+
+    public double getCarbratio() {
+        return carbratio;
+    }
+
+    public List<BasalProfile> getBasalProfiles() {
+        return basalProfiles;
+    }
+
+    public static class BasalProfile {
+        private LocalTime start;
+        private double value;
+
+        public BasalProfile(LocalTime start, double value) {
+            this.start = start;
+            this.value = value;
+        }
+
+        public LocalTime getStart() {
+            return start;
+        }
+
+        public double getValue() {
+            return value;
+        }
+    }
+}
---
+++ code/OpenDiabetes-NSApi/test/de/opendiabetes/vault/parser/TestProfileParser.java
@@ -0,0 +1,65 @@
+package de.opendiabetes.vault.parser;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestProfileParser {
+    @Test
+    public void randomProfile() {
+        Random random = new Random();
+        ZoneId zone = ZoneId.systemDefault();
+        double sensitivity = random.nextDouble() * 100;
+        double carbratio = random.nextDouble() * 100;
+        int basals = 1 + random.nextInt(9);
+        LocalTime[] basalTimes = new LocalTime[basals];
+        double[] basalValues = new double[basals];
+        String input = "[{" +
+                "  \"store\": {" +
+                "    \"Default\": {" +
+                "      \"sens\": [" +
+                "        {" +
+                "          \"time\": \"00:00\"," +
+                "          \"timeAsSeconds\": \"0\"," +
+                "          \"value\": \"" + sensitivity + "\"" +
+                "        }" +
+                "      ]," +
+                "      \"basal\": [";
+        for (int i = 0; i < basals; i++) {
+            basalTimes[i] = LocalTime.of(random.nextInt(24), random.nextInt(60));
+            basalValues[i] = random.nextDouble() * 2;
+            input += "{" +
+                    "\"time\": \"" + basalTimes[i].toString() + "\"," +
+                    "\"value\": \"" + basalValues[i] + "\"" +
+                    "}";
+            if (i < basals - 1)
+                input += ",";
+        }
+        input += "      ]," +
+                "      \"carbratio\": [" +
+                "        {" +
+                "          \"time\": \"00:00\"," +
+                "          \"timeAsSeconds\": \"0\"," +
+                "          \"value\": \"" + carbratio + "\"" +
+                "        }" +
+                "      ]," +
+                "      \"timezone\": \"" + zone.toString() + "\"" +
+                "    }" +
+                "  }" +
+                "}]";
+
+        Profile profile = new ProfileParser().parse(input);
+        assertEquals(zone, profile.getTimezone());
+        assertEquals(sensitivity, profile.getSensitivity());
+        assertEquals(carbratio, profile.getCarbratio());
+        assertEquals(basals, profile.getBasalProfiles().size());
+        for (int i = 0; i < basals; i++) {
+            assertEquals(basalTimes[i], profile.getBasalProfiles().get(i).getStart());
+            assertEquals(basalValues[i], profile.getBasalProfiles().get(i).getValue());
+        }
+    }
+}
