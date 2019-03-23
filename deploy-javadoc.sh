ff40fd87c799b53508fa6f05c2b683df3574c086
Edgxxar <edgxxar@gmail.com>
2019-03-23 19:26:22 +0100

    use https

--- deploy-javadoc.sh
+++ deploy-javadoc.sh
@@ -5,7 +5,7 @@ ant generate-javadoc
 cd doc
 
 git init
-git remote add javadoc git@github.com:TUDa-BP-11/opendiabetes-uam-heuristik.git
+git remote add javadoc https://github.com/TUDa-BP-11/opendiabetes-uam-heuristik.git
 git fetch --depth=1 javadoc gh-pages
 git add --all
 git commit -m "javadoc"
