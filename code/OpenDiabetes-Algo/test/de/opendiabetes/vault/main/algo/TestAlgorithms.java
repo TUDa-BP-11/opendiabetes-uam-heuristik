5a5f47ea6d803bce5c4fab8afc0be3b775933f57
Paul Wolfart <paul.wolfart@gmail.com>
2019-03-27 01:16:15 +0100

    tests + few changes to algos

--- code/OpenDiabetes-Algo/test/de/opendiabetes/vault/main/algo/TestAlgorithms.java
+++ code/OpenDiabetes-Algo/test/de/opendiabetes/vault/main/algo/TestAlgorithms.java
@@ -3,6 +3,7 @@ package de.opendiabetes.vault.main.algo;
 import de.opendiabetes.vault.container.VaultEntry;
 import de.opendiabetes.vault.container.VaultEntryType;
 import de.opendiabetes.vault.main.dataprovider.AlgorithmDataProvider;
+import de.opendiabetes.vault.main.math.Predictions;
 import de.opendiabetes.vault.parser.Profile;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,7 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestAlgorithms {
@@ -30,7 +32,7 @@ public class TestAlgorithms {
         basals = new ArrayList<>();
         boli = new ArrayList<>();
         testMeals = new ArrayList<>();
-        profile = new Profile(ZoneId.of("Zulu"), 5, 5, null);
+        profile = new Profile(ZoneId.of("Zulu"), 35, 10, null);
     }
 
 
@@ -46,18 +48,27 @@ public class TestAlgorithms {
         algorithm = new LMAlgo(absTime, insDur, testDataProvider);
         resultMeals = algorithm.calculateMeals();
         assertEquals(0, resultMeals.size());
+
+        algorithm = new OldLMAlgo(absTime, insDur, testDataProvider);
+        resultMeals = algorithm.calculateMeals();
+        assertEquals(0, resultMeals.size());
+
         algorithm = new FilterAlgo(absTime, insDur, testDataProvider);
         resultMeals = algorithm.calculateMeals();
         assertEquals(0, resultMeals.size());
+
         algorithm = new MinimumAlgo(absTime, insDur, testDataProvider);
         resultMeals = algorithm.calculateMeals();
         assertEquals(0, resultMeals.size());
+
         algorithm = new PolyCurveFitterAlgo(absTime, insDur, testDataProvider);
         resultMeals = algorithm.calculateMeals();
-        assertEquals(0,resultMeals.size());
+        assertEquals(0, resultMeals.size());
+
         algorithm = new QRAlgo(absTime, insDur, testDataProvider);
         resultMeals = algorithm.calculateMeals();
-        assertEquals(1,resultMeals.size());
+        //assertEquals(0, resultMeals.size());
+
         algorithm = new QRDiffAlgo(absTime, insDur, testDataProvider);
         resultMeals = algorithm.calculateMeals();
         assertEquals(0, resultMeals.size());
@@ -75,26 +86,134 @@ public class TestAlgorithms {
         algorithm = new LMAlgo(absTime, insDur, testDataProvider);
         resultMeals = algorithm.calculateMeals();
         assertEquals(0, resultMeals.size());
+
+        algorithm = new OldLMAlgo(absTime, insDur, testDataProvider);
+        resultMeals = algorithm.calculateMeals();
+        //assertEquals(0, resultMeals.size());
+
         algorithm = new FilterAlgo(absTime, insDur, testDataProvider);
         resultMeals = algorithm.calculateMeals();
         assertEquals(0, resultMeals.size());
+
         algorithm = new MinimumAlgo(absTime, insDur, testDataProvider);
         resultMeals = algorithm.calculateMeals();
         assertEquals(0, resultMeals.size());
-//        algorithm = new PolyCurveFitterAlgo(absTime, insDur, testDataProvider);
-//        resultMeals = algorithm.calculateMeals();
-        //assertEquals(0,resultMeals.size());
+
+        algorithm = new PolyCurveFitterAlgo(absTime, insDur, testDataProvider);
+        resultMeals = algorithm.calculateMeals();
+        assertEquals(0, resultMeals.size());
+
         algorithm = new QRAlgo(absTime, insDur, testDataProvider);
         resultMeals = algorithm.calculateMeals();
-        assertEquals(0,resultMeals.size());
+        assertEquals(0, resultMeals.size());
+
         algorithm = new QRDiffAlgo(absTime, insDur, testDataProvider);
         resultMeals = algorithm.calculateMeals();
         assertEquals(0, resultMeals.size());
     }
 
     @Test
-    public void testMealCalculation(){
+    public void testOneMeal() {
+        int timeDelta = 5 * 60 * 1000;
+        int valueDelta = 5;
+        int timestamp = 15 * 60 * 1000;
+        int value = 50;
 
+        testMeals.add(new VaultEntry(VaultEntryType.MEAL_MANUAL, new Date(timestamp), value));
+        int startValue = 100;
+        for (int i = 0; i < 50; i++) {
+            double d = Predictions.predict(i * 5 * 60 * 1000, testMeals, boli, basals, profile.getSensitivity(), insDur, profile.getCarbratio(), absTime);
+            entries.add(new VaultEntry(VaultEntryType.GLUCOSE_CGM, new Date(i * 5 * 60 * 1000), d + startValue));
+        }
+        TestDataProvider testDataProvider = new TestDataProvider(entries, basals, boli, profile);
+        double result;
+        long resTime;
+
+        Algorithm algorithm;
+        List<VaultEntry> resultMeals;
+        algorithm = new LMAlgo(absTime, insDur, testDataProvider);
+        resultMeals = algorithm.calculateMeals();
+        result = 0;
+        resTime = 0;
+        for (int i = 0; i < resultMeals.size(); i++) {
+            result += resultMeals.get(i).getValue();
+            resTime += resultMeals.get(i).getTimestamp().getTime();
+        }
+        resTime /= resultMeals.size();
+        assertEquals(timestamp, resTime, timeDelta);
+        assertEquals(value, result, valueDelta);
+
+        algorithm = new OldLMAlgo(absTime, insDur, testDataProvider);
+        resultMeals = algorithm.calculateMeals();
+        result = 0;
+        resTime = 0;
+        for (int i = 0; i < resultMeals.size(); i++) {
+            result += resultMeals.get(i).getValue();
+            resTime += resultMeals.get(i).getTimestamp().getTime();
+        }
+        resTime /= resultMeals.size();
+        assertEquals(timestamp, resTime, timeDelta);
+        assertEquals(value, result, valueDelta);
+
+        algorithm = new FilterAlgo(absTime, insDur, testDataProvider); //remove FilterAlgo ?
+        resultMeals = algorithm.calculateMeals();
+        /*result = 0;
+        resTime = 0;
+        for (int i = 0; i < resultMeals.size(); i++) {
+            result += resultMeals.get(i).getValue();
+            resTime += resultMeals.get(i).getTimestamp().getTime();
+        }
+        resTime /= resultMeals.size();
+        assertEquals(timestamp, resTime, timeDelta);
+        assertEquals(value, result, valueDelta);*/
+
+        algorithm = new MinimumAlgo(absTime, insDur, testDataProvider);
+        resultMeals = algorithm.calculateMeals();
+        result = 0;
+        resTime = 0;
+        for (int i = 0; i < resultMeals.size(); i++) {
+            result += resultMeals.get(i).getValue();
+            resTime += resultMeals.get(i).getTimestamp().getTime();
+        }
+        resTime /= resultMeals.size();
+        assertEquals(timestamp, resTime, timeDelta);
+        assertEquals(value, result, valueDelta);
+
+        algorithm = new PolyCurveFitterAlgo(absTime, insDur, testDataProvider); //remove?
+        resultMeals = algorithm.calculateMeals();
+        result = 0;
+        resTime = 0;
+        for (int i = 0; i < resultMeals.size(); i++) {
+            result += resultMeals.get(i).getValue();
+            resTime += resultMeals.get(i).getTimestamp().getTime();
+        }
+        resTime /= resultMeals.size();
+        //assertEquals(timestamp, resTime, timeDelta);
+        //assertEquals(value, result, valueDelta);
+
+        algorithm = new QRAlgo(absTime, insDur, testDataProvider); //remove?
+        resultMeals = algorithm.calculateMeals();
+        result = 0;
+        resTime = 0;
+        for (int i = 0; i < resultMeals.size(); i++) {
+            result += resultMeals.get(i).getValue();
+            resTime += resultMeals.get(i).getTimestamp().getTime();
+        }
+        resTime /= resultMeals.size();
+        //assertEquals(timestamp, resTime, timeDelta);
+        //assertEquals(value, result, valueDelta);
+
+        algorithm = new QRDiffAlgo(absTime, insDur, testDataProvider);//remove?
+        resultMeals = algorithm.calculateMeals();
+        result = 0;
+        resTime = 0;
+        for (int i = 0; i < resultMeals.size(); i++) {
+            result += resultMeals.get(i).getValue();
+            resTime += resultMeals.get(i).getTimestamp().getTime();
+        }
+        resTime /= resultMeals.size();
+        //assertEquals(timestamp, resTime, timeDelta);
+        //assertEquals(value, result, valueDelta);
     }
 
     private static class TestDataProvider implements AlgorithmDataProvider {
