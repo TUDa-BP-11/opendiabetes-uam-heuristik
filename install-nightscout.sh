c168e02b7d32e8fb7285779829be738b6fc6e304
Edgxxar <edgxxar@gmail.com>
2019-03-29 13:09:22 +0100

    derp

--- install-nightscout.sh
+++ install-nightscout.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# clone and enter directory
 git clone https://github.com/TUDa-BP-11/cgm-remote-monitor && cd cgm-remote-monitor
 git checkout -b uam
 git branch --set-upstream-to=origin/uam uam
@@ -10,5 +11,7 @@ npm install
 # start server in background
 npm run start > server.log 2>&1 &
 
+# go back to original directory
+cd ..
 # insert default profile
 mongo --eval "db.profile.insert(`cat profile.json`)" nightscout
